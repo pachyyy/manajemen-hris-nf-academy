ba10d0caa627bd6136cf91f8ced17e33d3f236de
SatryoRiezdiansyah <muhammadsatryo.23144@mhs.unesa.ac.id>
2025-11-08 11:17:58 +0700

    database data pegawai

---
+++ database/migrations/2025_11_08_031805_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('division');
+            $table->string('position');
+            $table->date('join_date');
+            $table->string('contact');
+            $table->enum('status', ['aktif', 'cuti', 'resign'])->default('aktif');
+            $table->text('document_path')->nullable();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
